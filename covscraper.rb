f01724eaaa752860b893e240b6de335d1e96cdfe
gdumani <gdumani@gmail.com>
2020-08-20 16:22:47 -0600

    methots to fecth list of regions an links to data

--- covscraper.rb
+++ covscraper.rb
@@ -7,10 +7,21 @@ def load_html(site,link)
   Nokogiri::HTML(page)
 end
 
+def name_link(reg)
+  name=reg.css('span').to_s.delete_prefix('<span>').delete_suffix('</span>')
+  ref= reg.map.to_h['href']
+  {name => ref}
+end
+
 uri='http://coronavirus.jhu.edu'
 link="/region"
 separator="div.RegionMenu_items__3D_d2"
 
 regions= load_html(uri,link).css(separator)
 
-puts regions.count
+us = regions[0].css('a').map {|r| name_link r}
+
+world = regions[1].css('a').map {|r| name_link r}
+
+puts us
+puts world
