3f3a9bc69141bb69f4b4f963c8b6797a86b87c30
gdumani <gdumani@gmail.com>
2020-08-23 06:04:07 -0600

    added watir code to open links to extract information fetched by scripts with browser

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # covidscraper
 John Hopkins University Covid 19 global statistics scraper
+
+requires 
+
+Ruby 2.6.5
+google chrome
+chromedriver 
+
+The following ruby gems:
+
+nokogiri
+watir
+rspec for testing
+
+
+installation of chromedriver:
+
+Follow instruction from: `https://chromedriver.chromium.org/`
+Quick tip: 
+After downloading the corresponding version for your Chrome browser, unpack the file and copy it to a location in your path or add the desired location to it.
--- covscraper.rb
+++ covscraper.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
-require 'byebug'
+require 'watir'
 
 def load_html(site,link)
   page = open(site + link)
@@ -8,7 +8,7 @@ def load_html(site,link)
 end
 
 def name_link(reg)
-  name=reg.css('span').to_s.delete_prefix('<span>').delete_suffix('</span>')
+  name=reg.css('span').to_s.gsub(/<[^>]+>/,'')
   ref= reg.map.to_h['href']
   {name => ref}
 end
@@ -25,6 +25,15 @@ regions[0].css('a').each {|r| us.merge!(name_link r)}
 world=Hash.new
 regions[1].css('a').each {|r| world.merge!(name_link r)}
 
+browser=Watir::Browser.new
+usdata=Hash.new
+us.each do |k,v|
+  browser.goto uri+v
+  data=((Nokogiri::HTML(browser.html)).css('div.RegionOverview_overviewBlock__32xzs')).to_a 
+  clean=Hash.new
+  data.each {|d| clean.merge!({d.css('h3').to_s.gsub(/<[^>]+>/,'').chop => d.css('span').to_s.gsub(/<[^>]+>/,'')})} 
+  usdata.merge!({k=>[{'Page link'=>v}.merge!(clean)]})
+end
 
 puts us
 puts world
